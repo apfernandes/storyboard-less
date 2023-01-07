3e6f84e7fa5d1c2c67f752cf0970615afc20d791
A. Pedro Fernandes <pedro.fernandes@ieee.org>
2023-01-07 21:55:08 +0000

    Add files via upload

---
+++ Xcode/storyboard-less/storyboard-less/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  storyboard-less
+//
+//  Created by Pedro Fernandes on 07/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.yellow
+    }
+
+
+}
+
